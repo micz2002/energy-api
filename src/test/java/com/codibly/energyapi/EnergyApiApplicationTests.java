2b45431794672c74619a6da66ab0009ced112e41
micz2002 <jakub2002pl@wp.pl>
2026-06-27 18:15:56 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/com/codibly/energyapi/EnergyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codibly.energyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

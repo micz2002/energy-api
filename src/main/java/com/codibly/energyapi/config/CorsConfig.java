ae266ba27e010c7be2ceeee538502c467584b854
micz2002 <jakub2002pl@wp.pl>
2026-07-02 18:54:55 +0200

    chore: configure CORS

---
+++ src/main/java/com/codibly/energyapi/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.codibly.energyapi.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig implements WebMvcConfigurer {
+
+    private final CorsProperties corsProperties;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(corsProperties.allowedOrigins().toArray(String[]::new))
+                .allowedMethods("GET")
+                .allowedHeaders("*");
+    }
+}
